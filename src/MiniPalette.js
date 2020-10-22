9ba799214edec4339438205e132e2e6d0f8ee46c
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-10-22 21:36:02 +0300

    Add initial mini palette styles

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,20 +2,46 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
-  main: {
+  root: {
     backgroundColor: "white",
-    "& h1": {
-      margin: "0",
+    border: "1px solid black",
+    padding: "0.5rem",
+    borderRadius: "5px",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer",
     },
+  },
+
+  colors: {
+    backgroundColor: "grey",
+  },
+
+  title: {
+    color: "black",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontSize: "1rem",
+    paddingTop: "0.5rem",
+    margin: "0",
+  },
+
+  emoji: {
+    fontSize: "1.5rem",
+    marginLeft: "0.5rem",
   },
 };
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
 
   return (
-    <div className={classes.main}>
-      <h1>Mini Palette</h1>
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <div className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </div>
     </div>
   );
 }
