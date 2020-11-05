52a090545b068a1ce25c9f9d80f923fed867a6bc
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-11-05 18:51:22 +0300

    Make palettes responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 export default {
   Palette: {
     height: "100vh",
@@ -33,6 +35,18 @@ export default {
       fontSize: "1rem",
       cursor: "pointer",
       textDecoration: "none",
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "33.3333%",
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "20%",
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: "10%",
     },
   },
 };
